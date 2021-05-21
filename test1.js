0acbaf17e133997e64ca094b85cc0acf37c8f868
Rejeev Divakaran <>
2021-05-21 14:20:47 +0000

    added run command in .replit

--- test1.js
+++ test1.js
@@ -1,1 +1,21 @@
 const _ = require("lodash");
+const moment = require("moment");
+
+let filters = {
+  abc: "abc",
+  xyz: "XYZ"
+}
+let tags = {
+  abc: "abc",
+  xyz: "XYZ",
+  pqr: "PQR"
+}
+
+let match = _.every(filters, function(value, key){
+  console.log("k=" + key + ",v=" + value);
+  return tags[key] === value;
+});
+
+console.log("match=" + match);
+
+
